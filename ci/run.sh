2ac8ec98d8ea68104172cb0881ea56f73f8ae6a6
gnzlbg <gonzalobg88@gmail.com>
2018-04-16 22:25:53 +0200

    fix typo in ci script

--- ci/run.sh
+++ ci/run.sh
@@ -5,9 +5,9 @@ cargo build --no-default-features
 ! find target/ -name *.rlib -exec nm {} \; | grep "std"
 
 cargo test --no-default-features
-cargo test --nodefault-features --features use_std
-cargo test --nodefault-features --features unstable
-cargo test --nodefault-features --features use_std,unstable
+cargo test --no-default-features --features use_std
+cargo test --no-default-features --features unstable
+cargo test --no-default-features --features use_std,unstable
 
 cargo test --no-default-features --release
-cargo test --nodefault-features --release --features use_std,unstable
+cargo test --no-default-features --release --features use_std,unstable
